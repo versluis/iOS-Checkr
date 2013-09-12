70af9ab710529779c8ad39bd3902b760cee90743
Jay Versluis <versluis@Jays-MacBook-Pro.local>
2013-09-12 19:30:32 -0400

    added checking for float value

--- iOS Checkr/ViewController.m
+++ iOS Checkr/ViewController.m
@@ -18,12 +18,28 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.versionLabel.text = [self checkVersion];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSString *)checkVersion {
+    
+    float version = [[[UIDevice currentDevice] systemVersion] floatValue];
+    
+    if (version >= 6.01) {
+        // iOS-6.01+ code
+    } else {
+        // prior iOS versions
+    }
+    
+    // turn into printable string
+    NSString *theVersion = [[NSString alloc]initWithFormat:@"%f", version];
+    return theVersion;
 }
 
 @end
